839b8f16026091965e8a17e531050fc865174c4b
julius.jann <julius.jann@fyber.com>
2016-04-22 13:19:08 +0200

    so much color

---
+++ lets-draw-oblong-colorful.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.color('blue', 'green')
+turtle.width(30)
+
+turtle.shape("turtle")
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.exitonclick()
