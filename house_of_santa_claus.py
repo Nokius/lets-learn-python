29ea1f5c22ebd5c192a9f034f951873c2e56d826
julius.jann <julius.jann@fyber.com>
2016-04-22 16:12:50 +0200

    one day there is time for solving it

--- house_of_santa_claus.py
+++ house_of_santa_claus.py
@@ -5,11 +5,28 @@ a = b = 50
 
 c = math.sqrt(a**2 + b**2)
 
-turtle.forward(c)
-turtle.left(135)
-turtle.forward(b)
+turtle.forward(0)
 turtle.left(90)
 turtle.forward(a)
+turtle.left(90)
+turtle.forward(a)
+turtle.left(90)
+turtle.forward(a)
+turtle.left(135)
+turtle.forward(c)
+turtle.left(90)
+turtle.forward(a)
+turtle.left(90)
+turtle.forward(b)
+turtle.left(90)
+#turtle.forward(c)
+
+#turtle.left(45)
+#turtle.forward(c)
+#turtle.left(135)
+#turtle.forward(c)
+#turtle.right(135)
+
 
 turtle.exitonclick()
 
