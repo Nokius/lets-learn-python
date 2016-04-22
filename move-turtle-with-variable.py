664fdef1dddb8333c07270526c41179737d53599
julius.jann <julius.jann@fyber.com>
2016-04-22 13:52:18 +0200

    lets move the navel

---
+++ move-turtle-with-variable.py
@@ -0,0 +1,13 @@
+import turtle
+
+navel = turtle
+
+navel.left(180)
+navel.forward(50)
+navel.right(90)
+navel.forward(50)
+navel.right(90)
+navel.forward(50)
+navel.right(90)
+navel.forward(50)
+navel.exitonclick()
