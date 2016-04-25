3813d573585026f68ca978fc92d04c95d7f92d1a
julius.jann <julius.jann@fyber.com>
2016-04-25 15:24:27 +0200

    I know what u sad

---
+++ this-is-what-u-sad.py
@@ -0,0 +1,13 @@
+
+word = ''
+sentence = ''
+
+print('Please enter some words')
+print('Include a period (.) when you are finished.')
+while '.' not in word:
+    word = input('next word: ')
+    sentence = word + ' ' + sentence
+print()
+print('Aha! You said: ')
+print(sentence)
+
