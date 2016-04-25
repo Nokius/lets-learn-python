11c3afd2f84f5797d39b24b5662636f7ee0cb31c
julius.jann <julius.jann@fyber.com>
2016-04-25 10:41:53 +0200

    hexagon and length as variable

--- draw-hexagon-diffrent-size.py
+++ draw-hexagon-diffrent-size.py
@@ -1,13 +1,15 @@
 import turtle
 
+a = 100
+
 def draw_hex(length):
     for _ in range(6):
         turtle.forward(length)
         turtle.left(60)
 
 for _ in range(6):
-    draw_hex(50)
-    turtle.forward(50)
+    draw_hex(a)
+    turtle.forward(a)
     turtle.right(60)
 
 turtle.exitonclick()
