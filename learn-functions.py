d8080f4396d5b029f2ac4268a9cb8be35c195f44
julius.jann <julius.jann@fyber.com>
2016-04-25 08:56:29 +0200

    Hi functions

---
+++ learn-functions.py
@@ -0,0 +1,15 @@
+import turtle
+
+def line_without_moving():
+   turtle.forward(50)
+   turtle.backward(50)
+
+line_without_moving()
+turtle.right(90)
+line_without_moving()
+turtle.right(90)
+line_without_moving()
+turtle.right(90)
+line_without_moving()
+
+turtle.exitonclick()
