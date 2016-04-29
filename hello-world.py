8f4c79a7a56cb2742f2f23a30a25e898d245e295
julius.jann <julius.jann@fyber.com>
2016-04-29 11:21:20 +0200

    hallo world

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# This program says Hi and ask for your Name
+
+
+print('Hi!')
+print('What is your name?')   #ask for the name
+myName = input()
+print('It is nice to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')   #ask for the age
+myAge = input()
+print('You been  ' + str(int(myAge) -1) + ' last year.')
