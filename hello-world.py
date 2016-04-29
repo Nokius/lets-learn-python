f4cd9180a10ab3502f28f2dc3b0bbbe5144ac9b6
julius.jann <julius.jann@fyber.com>
2016-04-29 16:36:03 +0200

    hallo world

--- hello-world.py
+++ hello-world.py
@@ -9,4 +9,4 @@ print('The length of your name is:')
 print(len(myName))
 print('What is your age?')   #ask for the age
 myAge = input()
-print('You been  ' + str(int(myAge) -1) + ' last year.')
+print('You been  ' + str(int(myAge) - 1) + ' last year.')
